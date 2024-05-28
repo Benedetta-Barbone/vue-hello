5f59b8988a0ab86d71f5bf3cac3b3bc23413a057
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-28 19:41:25 +0200

    inserita img

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {       
       return {
-        message : 'Hello vue!'
+        message : 'Hello vue!',
+        imageUrl : 'https://st.ilfattoquotidiano.it/wp-content/uploads/2018/12/11/roger905.jpg'
       }
     }
   }).mount('#app')
